addcc92e9d70ead5aeb106e1bb77d063f8576cd0
Michael <antenehnigussie1@yahoo.com>
2021-04-08 18:08:12 +0100

    Fixed a bug: "undefined!" output for old jobs. Modified component code (Headings). Modified css rules (App.css).

--- src/Entities.js
+++ src/Entities.js
@@ -7,19 +7,11 @@ export const Logo = ({ link }) => {
 };
 
 export const Headings = ({ company, isNew, isFeatured }) => {
-  let status,
-    featured = "";
-  if (isNew) {
-    status = "NEW";
-  }
-  if (isFeatured) {
-    featured = "FEATURED";
-  }
   return (
     <div class="headings">
-      <span class="company">{company}</span>
-      <span class={status}>{`${status}!`}</span>
-      <span class={featured}>{featured}</span>
+      <span className="company">{company}</span>
+      {isNew && <span className="new">NEW!</span>}
+      {isFeatured && <span className="featured">FEATURED</span>}
     </div>
   );
 };
